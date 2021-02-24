8cc05b919ff5a8ebcc4791b7f645c3447b7e5d0a
Divyanka Chaudhari <divyanka.chaudhari25@gmail.com>
2021-02-24 16:20:22 +0530

    lalalala

--- subtask-1/sub_task1.cpp
+++ subtask-1/sub_task1.cpp
@@ -1,4 +1,10 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <fstream>
+#include <regex>
 
 #define pb push_back
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
@@ -14,13 +20,12 @@ struct userdata {
     vector<Point2f> points;
 };
 
-
 void mouseHandler(int event, int x, int y, int flags, void* data_ptr) {
     if  (event == 1) {
         // Taking input of the mouse click
         userdata *data = ((userdata *) data_ptr);
         // Using small red dots/circles wherever the mouse clicks for user to know
-        circle(data->im, Point(x,y),2,Scalar(0,0,600), 5, cv::LINE_AA);
+        circle(data->im, Point(x,y),2,Scalar(0,0,255), 5, cv::LINE_AA);
         // To show the Image with Red dots
         imshow("Image", data->im);
         // Saving the co-ordinates of mouse click in data
@@ -37,6 +42,13 @@ int checkImage(bool check) {
       return -1;
   }
   return 0;
+}
+bool isImageFile(string str) {
+  // Regex to check valid image file extension.
+  const regex pattern("[^\\s]+(.*?)\\.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$");
+  if (str.empty()){ return false; }
+  if(regex_match(str, pattern)){ return true;}
+  else { return false;}
 }
 
 void cropImage(Mat &croppedImage, Mat &bird_view, vector<pair<int, int>> &mouse_clicks, vector<pair<int, int>> &crop_this, userdata &data, Mat &h) {
@@ -68,60 +80,88 @@ void destPoints(vector<Point2f> &pts_dst){
 
 }
 
+inline bool isFileExist (const std::string& name) {
+  struct stat buffer;
+  return (stat (name.c_str(), &buffer) == 0);
+}
+
+int checkFile(bool check1, bool check2) {
+  if (check1 ==false || check2 == false) {
+      cout << "Hold up! \nMission failed succesfully: Could not find that image, or maybe that file isn't an image. \nDid you check if that file exists in this directory?" << endl;
+      // wait for any key to be pressed
+      //cin.get();
+      return -1;
+  }
+  return 0;
+}
+
 int main( int argc, char** argv) {
-    fast;
+    //fast;
 // READING AND FINDING HOMOGRAPHY ---------------------------------------------
     // Read source image. Inputted from user.
-    string name = argv[1];
-    Mat im_src = imread(name);
-    Size size = im_src.size();
-    Mat im_dst = Mat::zeros(size,CV_8UC3);
-
-    // Create vector and add destination points to it
-    vector<Point2f> pts_dst;
-    destPoints(pts_dst);
-
-    // Set data for mouse event
-    Mat im_temp = im_src.clone();
-    userdata data;
-    data.im = im_temp;
-
-    cout << "Choose the point in anti-clockwise fashion, starting from top-left." << endl;
-
-    // Show image and wait for mouse clicks
-    imshow("Image", im_temp);
-    setMouseCallback("Image", mouseHandler, &data);
+    cout<<"Enter file name/destination: ";
+    //string name = argv[1];
+    string name;
     waitKey(0);
+    cin>> name;
 
-    // Calculate the homography & warp source image to destination
-    Mat h = findHomography(data.points, pts_dst);
-    warpPerspective(im_src, im_dst, h, size);
+    // Check if file exists; return if it doesn't
+    if(checkFile(isFileExist(name), isImageFile(name)) == -1) {return 0;}
 
-// SHOWING THE UNCROPPED IMAGE AND SAVING IT ----------------------------------
-    // Show image
-    imshow("Image", im_dst);
+    else {
+      Mat im_src = imread(name);
+      Mat im_temp = im_src.clone();
+      cvtColor(im_src, im_temp, COLOR_BGR2GRAY);
+      Size size = im_src.size();
+      Mat im_dst = Mat::zeros(size,CV_8UC3);
 
-    // saving the uncropped image
-    bool check1 = imwrite("birds_eye_view.jpg", im_dst);
-    checkImage(check1);
-	  cout << "Successfully saved the uncropped birds view image. Cropping it now." << endl;
+      // Create vector and add destination points to it
+      vector<Point2f> pts_dst;
+      destPoints(pts_dst);
 
-// CROPPING THE IMAGE, SHOWING AND SAVING IT-----------------------------------
-    // now crop
-    vector<pair<int, int>> mouse_clicks(4, {0, 0});
-    vector<pair<int, int>> crop_this(4, {0, 0});
-    Mat croppedImage;
-    Mat bird_view = imread("birds_eye_view.jpg", 1);
+      // Set data for mouse event
+      // Mat im_temp = im_src.clone();
+      userdata data;
+      data.im = im_temp;
+      // cvtColor(im_temp, im_temp, COLOR_BGR2GRAY);
+      // cvtColor(im_src, im_src,COLOR_BGR2GRAY);
+      cout << "Choose the point in anti-clockwise fashion, starting from top-left." << endl;
 
-    cropImage(croppedImage,bird_view, mouse_clicks, crop_this, data, h);
-    imshow("cropped_birds_view", croppedImage);
+      // Show image and wait for mouse clicks
+      imshow("Image", im_temp);
+      setMouseCallback("Image", mouseHandler, &data);
+      waitKey(0);
 
-    // Saving the cropped image
-    bool check2 = imwrite("cropped_birds_view.jpg", croppedImage);
-    checkImage(check2);
-    cout << "Successfully saved the cropped birds view image. \nMission accomplished." << endl;
+      // Calculate the homography & warp source image to destination
+      Mat h = findHomography(data.points, pts_dst);
+      warpPerspective(im_temp, im_dst, h, size);
 
-    waitKey(0);
+  // SHOWING THE UNCROPPED IMAGE AND SAVING IT ----------------------------------
+      // Show image
+      imshow("Image", im_dst);
 
-    return 0;
+      // saving the uncropped image
+      bool check1 = imwrite("birds_eye_view.jpg", im_dst);
+      checkImage(check1);
+  	  cout << "Successfully saved the uncropped birds view image. Cropping it now." << endl;
+
+  // CROPPING THE IMAGE, SHOWING AND SAVING IT-----------------------------------
+      // now crop
+      vector<pair<int, int>> mouse_clicks(4, {0, 0});
+      vector<pair<int, int>> crop_this(4, {0, 0});
+      Mat croppedImage;
+      Mat bird_view = imread("birds_eye_view.jpg", 1);
+
+      cropImage(croppedImage,bird_view, mouse_clicks, crop_this, data, h);
+      imshow("cropped_birds_view", croppedImage);
+
+      // Saving the cropped image
+      bool check2 = imwrite("cropped_birds_view.jpg", croppedImage);
+      checkImage(check2);
+      cout << "Successfully saved the cropped birds view image. \nMission accomplished." << endl;
+
+      waitKey(0);
+
+      return 0;
+  }
 }
