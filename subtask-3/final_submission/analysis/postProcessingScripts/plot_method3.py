45417db61ec89f86f056eb7b7784ad087d2be8d9
Kshitiz-Bansal <55652844+Kshitiz-Bansal@users.noreply.github.com>
2021-04-02 14:26:58 +0530

    Create plot_method3.py

---
+++ subtask-3/final_submission/analysis/postProcessingScripts/plot_method3.py
@@ -0,0 +1,17 @@
+import pandas as pd
+data = pd.read_csv('time.txt',sep=' ',header=None)
+data = pd.DataFrame(data)
+
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 22})
+x = data[0]
+y = data[1]
+z = data[2]
+plt.plot(x, y,'ro-', label = "CPU Clock Runtime")
+plt.plot(x, z,'go-', label = "Wall Time")
+plt.xlabel('Parameter (Num of threads)')
+plt.ylabel('Time')
+plt.title("Method3: Time VS Parameter")
+plt.legend()
+
+plt.show()
